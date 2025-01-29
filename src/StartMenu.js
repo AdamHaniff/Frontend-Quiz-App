3e50e0e4f5d25be3e6c96de27471b4f14d123671
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2025-01-28 23:55:45 -0500

    Added in light theme colors to design system

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -5,14 +5,17 @@ function StartMenu() {
   return (
     <div className="start-menu">
       <div className="greeting">
-        <h1 className="greeting__text">Welcome to the Frontend Quiz!</h1>
+        <h1 className="greeting__text">
+          Welcome to the
+          <span className="greeting__text--medium-bold">Frontend Quiz!</span>
+        </h1>
         <span className="greeting__instruction">
           Pick a subject to get started.
         </span>
       </div>
       <div className="start-menu__subjects">
         {data.quizzes.map((quiz) => (
-          <div className="subject-container">
+          <div className="subject-container" key={quiz.title}>
             <Subject quizObj={quiz} />
           </div>
         ))}
